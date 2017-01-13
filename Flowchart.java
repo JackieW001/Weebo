9f4113a11ae47e3317fca164ec450689d339b172
whyCantIUseMyNonStuyeduAccount <nborisov@stuy.edu>
2017-01-13 16:48:47 -0500

    updated to better insert string and begin actuall dvelopment

--- Flowchart.java
+++ Flowchart.java
@@ -3,67 +3,42 @@ public class Flowchart{
     private String[][] grid;
     //constructor
     public Flowchart(){
-        grid = new String[20][100];
+        grid = new String[10][30];
         for(int i = 0; i < grid.length; i+=1){
             for(int j = 0; j < grid[i].length; j+=1){
-                grid[i][j] = " ";
+                grid[i][j] = "_";
             }
         }            
     }
-    
+
+    //
     //setGridElem
-    public void setSpot(int i, int j, String str){
-        grid[i][j] = str;
+    public void setCell(int r, int c, String str){
+        grid[r][c] = str;
     }
-    //single loop filler
-    public void fill(ArrayList input){
-        int boolLength = ((String)input.get(1)).substring(9).length();
-        int updaLength = ((String)input.get(2)).substring(9).length();
-        int initLength = ((String)input.get(0)).substring(9).length();
-        //
-        setSpot(0,5,"|");
-        setSpot(1,5, ((String)input.get(0)).substring(9));
-        setSpot(2,5,"|");
-        setSpot(3,5,"V");
-        //
-        setSpot(4,0,"-");
-        setSpot(4,1,"-");
-        setSpot(4,2,"-");
-        setSpot(4,3,"-");
-        setSpot(4,4,"<");
-        setSpot(4,5, ((String)input.get(1)).substring(9));
-        setSpot(4,6,">");
-        setSpot(4,7,"-");
-        setSpot(4,8,"-");
-        setSpot(4,9,"-");
-        setSpot(4,10,"-");
-        setSpot(4,11,"-");
-        //
-        setSpot(5,0,"|");
-        setSpot(6,0,"no");
-        setSpot(7,0,"|");
-        setSpot(8,0,"V");
-        setSpot(9,0,"exit");
-        //
-        setSpot(5,13+initLength,"|");
-        setSpot(6,13 + initLength ,"yes");
-        
-        setSpot(7,6 + max(updaLength+1,initLength),"|");
-        setSpot(8,13 + initLength ,"V");
-        //
-        setSpot(9,7,"[");
-        setSpot(9,8, ((String)((ArrayList)input.get(3)).get(0)).substring(9));
-        setSpot(9,9,"]");
-        //
-        setSpot(9,6,"-");
-        setSpot(9,5,"-");
-        setSpot(9,4,"-");
-        setSpot(9,3,"-");
-        setSpot(8,6,"|");
-        setSpot(7,6, ((String)input.get(2)).substring(9));
-        setSpot(6,5,"|");
-        setSpot(5,6,"^");
+    //fills in cells with single character strings of an input string to the left of the input cell
+    public void enterStringHorz(int r, int c, String str){
+	for(int i = 0; i < str.length(); i += 1){
+	    setCell(r, c+i, str.substring(i,i+1));
+	}
     }
+    //fills in cells with single character strings of an input string below the input cell
+    public void enterStringVert(int r, int c, String str){
+        for(int i = 0; i < str.length(); i += 1){
+	    setCell(r+i, c, str.substring(i,i+1));
+	}
+    }
+
+    //
+    //inserts a downwards arrow stating the inititalization statment
+    public void initArrow(int r, int c, String init){
+	setCell(r,c,"|");
+	enterStringHorz(r+1,c,init);
+	enterStringVert(r+2,c,"|V");
+    }
+    //inserts a 
+    
+    //
     //printer
     public void printGrid(){
         String rowStr;
@@ -75,9 +50,10 @@ public class Flowchart{
             System.out.println(rowStr);
         }
     }
-    //max
-    public static int max(int a, int b){
-        if(a<b) return b;
-        else return a;
+    //main for now
+    public static void main(String args[]){
+	Flowchart blarg = new Flowchart();
+        blarg.initArrow(0,0,"int i = 0");
+	blarg.printGrid();
     }
 }
